fcd82e9d20001ff418186a0531aa5e7b067655a2
sophWillis <sophiewillis623@gmail.com>
2022-03-07 15:57:58 +0000

    feat: text with gradient

--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
-import './style.css'
+import "./style.css";
+
+const dpr = window.devicePixelRatio || 1;
 
 function setupCanvas(canvas) {
   // Get the device pixel ratio, falling back to 1.
-  var dpr = window.devicePixelRatio || 1;
   // Get the size of the canvas in CSS pixels.
   var rect = canvas.getBoundingClientRect();
   // Give the canvas pixel dimensions of their CSS
   // size * the device pixel ratio.
   canvas.width = rect.width * dpr;
   canvas.height = rect.height * dpr;
-  var ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext("2d");
   // Scale all drawing operations by the dpr, so you
   // don't have to worry about the difference.
   ctx.scale(dpr, dpr);
@@ -23,13 +24,32 @@ var canvas = document.getElementById("text-canvas");
 var ctx = setupCanvas(canvas);
 // var ctx = canvas.getContext("2d");
 
-ctx.font = `700 100px Montserrat`;
+let offset = 0;
+let speed = 0.01;
+
+function updateGrad() {
+  if (offset <= 0.9) {
+    offset = offset + speed;
+  }
+
+  const grad = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+
+  grad.addColorStop(0, "white");
+  grad.addColorStop(offset, "rgba(255,255,255,0)");
+
+  ctx.fillStyle = grad;
+
+  console.log(grad);
+}
+
+ctx.font = `700 50px Montserrat`;
+ctx.textBaseline = "middle";
 ctx.textAlign = "center";
 
-var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-gradient.addColorStop("0"," white");
-gradient.addColorStop("1.0", "transparent");
+function draw() {
+  updateGrad();
+  ctx.fillText("RETAKE", canvas.width / (2 + dpr), canvas.height / (2 + dpr));
+  requestAnimationFrame(draw);
+}
 
-ctx.fillStyle = gradient;
-
-ctx.fillText("RETAKE", canvas.width / 2, canvas.height / 2);
+draw();
